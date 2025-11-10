34f465645a9c001d0407c93b80544bc6e8f0b99e
PranitaPaudel <pranitapaudel67@gmail.com>
2025-11-10 09:33:40 +0545

    fix: fix error

--- src/app/hooks/AppRouter.tsx
+++ src/app/hooks/AppRouter.tsx
@@ -3,15 +3,20 @@ import Layout from "@app/hooks/Layout";
 import Homepage from "@home/Homepage";
 import ResumePage from "@resume/ResumePage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Homepage /> },
+        { path: "resume", element: <ResumePage /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { index: true, element: <Homepage /> },
-      { path: "resume", element: <ResumePage /> },
-    ],
-  },
-]);
+    basename: "/MyPortfolio",
+  }
+);
 
 export const AppRouter = () => <RouterProvider router={router} />;
